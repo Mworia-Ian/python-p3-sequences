25032d26bc91cb9582365a227a92fdc3387f7e04
ianmmworia <ianmworia156@gmail.com>
2024-05-29 08:09:49 +0300

    Assignment complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("The Fibonacci sequence of length 0 is an empty list.")
+        return
+
+    first = 0
+    second = 1
+    print(first, end=" ")
+
+    if length >= 2:
+        print(second, end=" ")
+        length -= 2
+    else:
+        return
+
+    while length > 0:
+        print(first + second, end=" ")
+        temp = second
+        second = first + second
+        first = temp
+        length -= 1
+
+print_fibonacci(8)
